5757563cae2a3a6ec10ddc74a8f223b83180d734
Magnanimoose <tomlaw172@gmail.com>
2022-02-15 16:47:06 -0400

    First Commit

---
+++ portfoliosite/homepage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views 
+from django.urls import reverse
+
+urlpatterns = [
+    path("", views.index, name = "index"),
+]
---
+++ portfoliosite/homepage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from typing import ChainMap
+from django.http.response import HttpResponseNotModified
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string 
+from django.http import Http404
+
+# Create your views here.
+def index(request):
+    return  render(request, "homepage/index.html")
